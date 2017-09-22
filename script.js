e1d19b595e200adac81a910286fa423d90b00352
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-09-21 23:10:01 -0400

    added media queries for mobile response

--- script.js
+++ script.js
@@ -10,14 +10,13 @@ function showSlides() {
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
+var toggleShow = function(){
 
-$(".show").on("click", function(){
-        $("a").siblings().toggle(1000);
-    });
-
-
+};
 
 // on click add id show to specific div and show hidden div
+// function expressions can not be called before actual expression since they are not hoisted
+toggleShow();
