ca263967c9671d0e74d2409b9180dba615e0e25e
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-12-18 22:41:16 -0500

    added grunt, and compressed images

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  runt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'small',
+            width: '30%',
+            suffix: '_small',
+            quality: 20
+          },{
+            name: 'large',
+            width: '50%',
+            suffix: '_large',
+            quality: 40
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png,jpeg}'],
+          cwd: 'images/',
+          dest: 'images_src/'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ["responsive_images"]);
+};
