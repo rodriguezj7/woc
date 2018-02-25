f8d28fad7e0ffd8cd962cd2ec77521ef4c2d13fc
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-02-24 23:02:21 -0500

    1dfdfsf

---
+++ test/js.js
@@ -0,0 +1,17 @@
+let modal = document.querySelector(".modal");
+let trigger = document.querySelector(".trigger");
+let closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
