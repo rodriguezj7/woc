3979285b96ea4a008e20ecff11d7d5cefb6eda42
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-02-25 23:17:20 -0500

    added two bios pages for two members, will need to do some formatting

--- test/js.js
+++ test/js.js
@@ -1,10 +1,23 @@
 let modal = document.querySelector(".modal");
-let trigger = document.querySelector(".trigger");
+let trigger = document.getElementsByClassName("trigger");
 let closeButton = document.querySelector(".close-button");
 
-function toggleModal() {
+const userInfo = {
+    "members": {
+
+    }
+};
+
+function toggleModal (e){
+    modal.classList.toggle("show-modal");
+    alert(this.id);
+
+}
+
+function closeModal() {
     modal.classList.toggle("show-modal");
 }
+
 
 function windowOnClick(event) {
     if (event.target === modal) {
@@ -12,6 +25,12 @@ function windowOnClick(event) {
     }
 }
 
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
+console.log(trigger);
+
+Array.from(trigger).forEach(function(element) {
+    let id = element.id;
+    element.addEventListener("click", toggleModal);
+});
+// trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", closeModal);
 window.addEventListener("click", windowOnClick);
